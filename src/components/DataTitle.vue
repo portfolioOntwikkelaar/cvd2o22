d900f31c3750dfb8db01c86ca69e05f0669f3de4
noordseoostende <voropayev1@mail.ru>
2022-08-04 19:14:22 +0200

    39:08

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center">
+  <h2 class="text=3xl font-bold">{{text}}
+    <div class="text-2xl mt-4 mb-10" >
+    {{timestamp}}</div>
+  </h2>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'DataTitle',
+  props: ['text', 'dataDate'],
+  computed: {
+    timestamp: function() {
+      return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
+    },
+  },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="home">
-    cvdapi
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats"/>
+    <CountrySelect :countries="countries" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+  <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+  <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
-
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'HomeView',
   components: {
-    
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
   },
   methods: {
      async fetchCovidData() {
@@ -21,7 +41,10 @@ export default {
   },
   async created() {
     const data = await this.fetchCovidData()
-    console.log(data)
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
   },
 }
 </script>
