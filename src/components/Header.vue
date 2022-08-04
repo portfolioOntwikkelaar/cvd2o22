1060851004afcf079c1835c069ff571807cf691e
noordseoostende <voropayev1@mail.ru>
2022-08-04 18:15:36 +0200

    clg data

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-yellow-800 text-white p-4 mb-10">
+  <div class=" text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i> Covid-2022
+  </div>
+  <p>find more on <a class="text-yellow-300" href="https://covid19api.com" target="_blank">covid19api.com</a>
+  </p></header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
